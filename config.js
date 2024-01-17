64dd1c602e24a26ef12cdffb1645d812b6386337
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2024-01-18 02:42:19 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+
+const CLIENT_ID = "285203164109-vt5rauutmo1vlhnseus2cma5nb3p9deg.apps.googleusercontent.com";
+
+const CLIENT_SECRET = "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";
+
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+
+const REFRESH_TOKEN = "1//04FCH2bCU1mGGCgYIARAAGAQSNwF-L9IrSM5o0OvOPm0fzok0INI3qfb-oK_3_8O40uUQjBp1e5HuCOC8SxEsbfTUHwKzoQBglaY";
+
+
+module.exports = { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
